9aef88a1696b4be161029a55edb30b7cb915cac8
Roman Rey <rey.roman23@gmail.com>
2022-11-12 15:30:46 -0500

    ActiveSession commit

---
+++ src/app/modelos/usuario.model.ts
@@ -0,0 +1,9 @@
+export class UsuarioModel{
+    id?: String;
+    nombre?: String;
+    apellidos?: string;
+    telefono?: string;
+    correo?: string;
+    token?: string;
+    isLoggedIn?: boolean = false;
+  }
